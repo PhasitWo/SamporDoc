d80a0ad41225f284b10c68d8652f3dd2448d0e78
PhasitWo <vrzoearth@gmail.com>
2026-03-03 19:38:29 +0700

    fix: open file on Windows11 by using Windows native API

--- openfile_windows.go
+++ openfile_windows.go
@@ -3,16 +3,21 @@
 package main
 
 import (
-	"os/exec"
-	"syscall"
+	"golang.org/x/sys/windows"
 )
 
 func (a *App) CMDOpenFile(filePath string) error {
-	var cmd *exec.Cmd
-	cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", filePath)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		HideWindow:    true,
-		CreationFlags: 0x08000000,
-	}
-	return cmd.Run()
+	verbPtr, _ := windows.UTF16PtrFromString("open")
+	pathPtr, _ := windows.UTF16PtrFromString(filePath)
+
+	err := windows.ShellExecute(
+		0,                     // Parent window handle (0 for none)
+		verbPtr,               // The action to perform
+		pathPtr,               // The file path
+		nil,                   // Parameters (used if opening an .exe)
+		nil,                   // Working directory (nil uses current)
+		windows.SW_SHOWNORMAL, // How to show the window
+	)
+
+	return err
 }
